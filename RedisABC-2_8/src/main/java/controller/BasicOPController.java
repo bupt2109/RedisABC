464e59ec8a85a30d1cfa6ba4504d87d5eb5eaf54
bupt2109 <565865732@qq.com>
2020-04-07 01:17:04 +0800

    完善connect过程

--- RedisABC-2_8/src/main/java/controller/BasicOPController.java
+++ RedisABC-2_8/src/main/java/controller/BasicOPController.java
@@ -2,17 +2,16 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -27,6 +26,8 @@ import java.util.ResourceBundle;
 public class BasicOPController implements Initializable {
 
     private static final String MSG_NO_CONNECT = "未连接Redis服务器";
+    private static final String IN = "<- ";
+    private static final String OUT = "-> ";
 
     @FXML
     private TextField ipText;
@@ -38,14 +39,44 @@ public class BasicOPController implements Initializable {
     private Button connBth;
     @FXML
     private ComboBox<Integer> selectBox;
+    @FXML
+    private ComboBox<String> infoBox;
+    @FXML
+    private TextArea historyData;
+    @FXML
+    private TabPane mainTab;
+    @FXML
+    private Tab infoTab;
+    @FXML
+    private TextArea infoArea;
 
-    private String ip;
-    private int port;
-    private String password;
-    private int db;
+    @FXML
+    private TextField stringGetKey;
+    @FXML
+    private TextField stringGetValue;
+    @FXML
+    private TextField stringGetDll;
+
+    private String ip;      //IP
+    private int port;       //端口
+    private String password;//redis AUTH密码
+    private int db;         //数据库0-15
+    private String version; //redis version
 
     private Jedis jedis = null;
     private boolean connected;
+    private final String[] INFO_SECTIONS = {
+            "All",
+            "Server",
+            "Clients",
+            "Memory",
+            "Persistence",
+            "Stats",
+            "Replication",
+            "CPU",
+            "Keyspace",
+            "Commandstats",
+    };
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -57,26 +88,18 @@ public class BasicOPController implements Initializable {
         selectBox.setVisibleRowCount(5);
         selectBox.setEditable(false);
         db = 0;
+        infoBox.setItems(FXCollections.observableArrayList(Arrays.asList(INFO_SECTIONS)));
+//        selectBox.setVisibleRowCount(5);
+        infoBox.setEditable(false);
     }
 
 
     public void connRedis(ActionEvent actionEvent) {
-        ip = ipText.getText();
-        port = Integer.parseInt(portText.getText());
-        password = passwordText.getText();
-        jedis = new Jedis(ip, port);
-        if (password != null && password.length() > 0) {
-            jedis.auth(password);
+        if(connected){
+            closeConn();
+        }else{
+            openConn();
         }
-        String res = jedis.ping();
-        if (res.equals("PONG")) {
-            connected = true;
-            connBth.setText("断开连接");
-            info(res);
-        } else {
-            error("连接失败");
-        }
-
 //        jedis = new Jedis("localhost");
 //        JedisPoolConfig pool = new JedisPoolConfig();
     }
@@ -86,14 +109,118 @@ public class BasicOPController implements Initializable {
         if (connected) {
             if (selectBox.getValue() != db) {
                 db = selectBox.getValue();
+                println(OUT + "SELECT:" + db);
                 String res = jedis.select(db);
-                info(res);
+//                info(res);
+                println(IN + res);
             }
         } else {
             warn(MSG_NO_CONNECT);
         }
     }
 
+    public void selectInfoTab(Event event) {
+        if(connected && infoTab.isSelected()){
+            println(OUT + "INFO");
+            infoArea.clear();
+            infoArea.appendText(jedis.info());
+        }
+    }
+
+    /**
+     * info指令切换section操作
+     * @param actionEvent
+     */
+    public void changeInfoBox(ActionEvent actionEvent) {
+        infoArea.clear();
+        if(connected) {
+            //TODO info返回值解析，可参考https://www.cnblogs.com/wshenjin/p/11431378.html
+            String section = infoBox.getValue();
+            println(OUT + "INFO " + section);
+            infoArea.appendText(jedis.info(section));
+        }
+    }
+
+    public void stringGetAction(ActionEvent actionEvent) {
+        if(connected){
+            String key = stringGetKey.getText();
+            if(key == null || key.isEmpty()){
+                warn("请输入查询值");
+            }else{
+                println(OUT + "GET "+key);
+                String res = jedis.get(key);
+                println(IN + res);
+                if(res != null){
+                    stringGetValue.setText(res);
+                }
+            }
+        }
+    }
+
+    public void openConn(){
+        ip = ipText.getText();
+        port = Integer.parseInt(portText.getText());
+        password = passwordText.getText();
+        jedis = new Jedis(ip, port);
+        println(OUT + "建立连接"+ip+":"+port);
+        try{
+            jedis.connect();
+        }catch (Exception ex){
+            error("建立连接失败:" + ex.getMessage());
+            return;
+        }
+        //密码效验
+        if (password != null && password.length() > 0) {
+            try{
+                println(OUT + "AUTH:"+password);
+                String pwRes = jedis.auth(password);
+                println(IN + pwRes);
+            }catch (Exception ex){
+                error("密码效验失败:" + ex.getMessage());
+                jedis.close();
+                return;
+            }
+        }
+        //测试PING
+        String pingResult = null;
+        try{
+            println(OUT + "PING");
+            pingResult = jedis.ping();
+        }catch (Exception ex){
+            error("PING测试失败:" + ex.getMessage());
+            jedis.close();
+            return;
+        }
+        if (pingResult.equals("PONG")) {
+            println(IN + pingResult);
+            connected = true;
+            connBth.setText("断开连接");
+            info("连接redis成功！");
+        } else {
+            error("PING测试失败:" + pingResult);
+            jedis.close();
+        }
+    }
+
+    public void closeConn(){
+        if(connected){
+            jedis.close();
+            connected = false;
+            connBth.setText("连接");
+            println(OUT + "关闭Redis连接");
+        }else{
+            warn(MSG_NO_CONNECT);
+        }
+    }
+
+
+    private void print(String msg){
+        historyData.appendText(msg);
+    }
+
+    private void println(String msg){
+        print(msg + "\n");
+    }
 
     private void info(String msg) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
